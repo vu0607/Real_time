c4d8149f35170eb898c006e4bd71aeb29182be23
vudangitwork <91715618+vu0607@users.noreply.github.com>
2022-06-08 21:56:03 +0700

    Add files via upload

---
+++ Ftech_test.py
@@ -0,0 +1,7 @@
+a = [1,-1,3,-1,0,-2]
+arrayMax =[]
+for i in range(1,len(a)+1):      #Su dung bien i de nhom i phan tu voi nhau
+        for j0 in range(len(a)-(i-1)):    
+            arrayMax.append(sum(a[j0:j0+i]))   # Tinh tong cua cac phan tu va gan no vao mang arrayMax
+Sum = max(arrayMax)
+print("Gia tri lon nhat cua mang con la ", Sum)
