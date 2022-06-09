0e2f08dd610855ed1ea2514dcd75854f21a24cab
vudangitwork <91715618+vu0607@users.noreply.github.com>
2022-06-09 23:15:34 +0700

    Add files via upload

--- test.py
+++ test.py
@@ -1,13 +1,7 @@
-import math
-def MinOp(N):
-    if (N <= 1):
-        return N;
- 
-    # Stores count of
-    # bits in N
-    bit = (int)(math.log(N) / math.log(2)) + 1
- 
-    # Recurrence relation
-    return ((1 << bit) - 1) - MinOp(N - (1 << (bit - 1)))
- 
-print(MinOp(5))
+a = [1,2,3,-1,0]
+arrayMax =[]
+for i in range(1,len(a)+1):      #Su dung bien i de nhom i phan tu voi nhau
+        for j0 in range(len(a)-(i-1)):    
+            arrayMax.append(sum(a[j0:j0+i]))   # Tinh tong cua cac phan tu va gan no vao mang arrayMax
+Sum = max(arrayMax)
+print("Gia tri lon nhat cua mang con la ", Sum)
