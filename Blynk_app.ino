cbb5de4be3cefc64c8b30f21f118e05501f14694
vudangitwork <91715618+vu0607@users.noreply.github.com>
2022-06-26 22:09:41 +0700

    Add files via upload

--- Blynk_app.ino
+++ Blynk_app.ino
@@ -2,7 +2,7 @@
 #define BLYNK_DEVICE_NAME           "Robot"
 #define BLYNK_AUTH_TOKEN            "F5kA7akB3_W2iqMIT9pBgRQTpPwXgvF3"
 
-
+#define sensor D0
 // Comment this out to disable prints and save space
 #define BLYNK_PRINT Serial
 
@@ -13,10 +13,10 @@ char auth[] = BLYNK_AUTH_TOKEN;
 
 // Your WiFi credentials.
 // Set password to "" for open networks.
-char ssid[] = "NokiaLumia";
-char pass[] = "99999999";
-int data;
-
+char ssid[] = "vu0607";
+char pass[] = "06072000";
+int i;
+int zone;
 WidgetLED ledConnect(V0);
 WidgetLED zone1(V1);
 WidgetLED zone2(V2);
@@ -36,42 +36,42 @@ void blinkLedWidget()
   }
 }
 
-void zone1Display(int state){
-  if(state == 1){
+void zone1Display(int state) {
+  if (state == 1) {
     zone1.on();
-  }else if(state == 0){
+  } else if (state == 0) {
     zone1.off();
   }
 }
 
-void zone2Display(int state){
-  if(state == 1){
+void zone2Display(int state) {
+  if (state == 1) {
     zone2.on();
-  }else if(state == 0){
+  } else if (state == 0) {
     zone2.off();
   }
 }
 
-void zone3Display(int state){
-  if(state == 1){
+void zone3Display(int state) {
+  if (state == 1) {
     zone3.on();
-  }else if(state == 0){
+  } else if (state == 0) {
     zone3.off();
   }
 }
 
-void zone4Display(int state){
-  if(state == 1){
+void zone4Display(int state) {
+  if (state == 1) {
     zone4.on();
-  }else if(state == 0){
+  } else if (state == 0) {
     zone4.off();
   }
 }
 
-void zone5Display(int state){
-  if(state == 1){
+void zone5Display(int state) {
+  if (state == 1) {
     zone5.on();
-  }else if(state == 0){
+  } else if (state == 0) {
     zone5.off();
   }
 }
@@ -80,6 +80,7 @@ void setup()
 {
   // Debug console
   Serial.begin(9600);
+  i = 0;
 
   Blynk.begin(auth, ssid, pass);
   // You can also specify server:
@@ -91,13 +92,36 @@ void setup()
 
 void loop()
 {
-  data = Serial.read(); //Read the serial data and store it
-  delay(1000);
-  if( data == 1 ) {
-    zone1Display(1);
-    }
-
-  
   Blynk.run();
-  timer.run();  
+  timer.run();
+  if(digitalRead(sensor) == 1) {
+    i = i + 1
+    while(digitalRead == 1) {}
+    }
+  if ( i == 0 ) {
+    zone1Display(0);
+    zone2Display(0);
+    zone3Display(0);
+    zone4Display(0);
+    zone5Display(0);
+  }
+  if ( i == 1 ) {
+    zone1Display(1);
+  }
+
+  if ( i == 2 ) {
+    zone2Display(1);
+  }
+  if ( i == 3 ) {
+    zone3Display(1);
+  }
+  if ( i == 4 ) {
+    zone4Display(1);
+  }
+  if ( i == 5 ) {
+    zone5Display(1);
+  }
+  delay(50);
+  Blynk.run();
+  timer.run();
 }
